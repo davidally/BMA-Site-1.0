7cb3462e98dae5ae437ad6cf6b601776cf796c80
david ally <davidally94@gmail.com>
2018-03-15 11:21:39 -0400

    js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-$( ".cordbtn" ).mouseover(function(){
-   $( ".cordbtn" ).trigger( "click" );
-});
+$(document).ready(function () {
+	$(".cordbtn").mouseover(function () {
+		$(".cordbtn").trigger("click");
+	});
+	$(".cordbtn").mouseleave(function () {
+		$(".cordbtn").trigger("click");
+	});
 });
